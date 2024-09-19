c89d84a2b54b045d648d7030f960a5d9042ad9cf
rakibmohammedii <rakibmohammed05@gmail.com>
2024-09-19 16:18:19 +0600

    added addmoney button

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('home added')
+// add money to the account
+
+
+// s1- add an  event handler and prevent page reload after form submit
+// s2- get money to be added to the account balance
+// s3- get the pin number provided
+// step1- add an event handler to the money button inside the Form 
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault;
+
+    // step2-get money to be added to the account balance
+    const addMoney = document.getElementById('input-add-money').value;
+    console.log(addMoney)
+    // s3- get the pin number provide
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+})
